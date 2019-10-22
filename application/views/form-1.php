b7abd28fbd4ce38768649a81b5c7fde3fea07adb
janpaul13 <56645190+janpaul13@users.noreply.github.com>
2019-10-22 10:01:26 -0500

    arnold commit

---
+++ application/views/form-1.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+
+<body style="background: #a1a1a1">
+	<div class="container" style="background: #fff">
+
+		<form>
+
+
+			<div class="row">
+
+				<div class="row">
+					<div class="input-field col s2">
+						<input id="numorden" type="text" class="validate">
+						<label for="numorden">N°Orden</label>
+					</div>
+				
+				</div>
+
+
+				<div class="row">
+					<div class="input-field col s2">
+						<input id="ApelliPaterno" type="text" class="validate">
+						<label for="ApelliPaterno">Apellido Paterno</label>
+					</div>
+				
+					<div class="input-field col s2">
+						<input id="ApelliMaterno" type="text" class="validate">
+						<label for="ApelliMaterno">Apellido Materno</label>
+					</div>
+	
+					<div class="input-field col s2">
+						<input id="Nombres" type="text" class="validate">
+						<label for="Nombres">Nombres</label>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="input-field col s2">
+						<input id="n_orden" type="text" class="validate">
+						<label for="N°Orden">N°Orden</label>
+					</div>
+				</div>
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				<div class="row">
+					<div class="input-field col s2">
+						<input placeholder="Apellido Paterno" id="Apellido_Paterno" type="text" class="validate">
+						<label for="Apellido_Paterno"></label>
+					</div>
+
+					<div class="input-field col s2">
+						<input placeholder="Apellido Materno" id="Apellido_Materno" type="text" class="validate">
+						<label for="Apellido_Materno"></label>
+					</div>
+
+					<div class="input-field col s2">
+						<input placeholder="Nombres" id="Nombres" type="text" class="validate">
+						<label for="Nombres"></label>
+					</div>
+
+					<div class="input-field col s2">
+						<input id="Numero_Historia" type="text" class="validate">
+						<label for="Num_historia">SASMDKASMD</label>
+					</div>
+
+
+
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input disabled value="I am not editable" id="Apellido" type="text" class="validate">
+						<label for="disabled">Disabled</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input id="password" type="password" class="validate">
+						<label for="password">Password</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input id="email" type="email" class="validate">
+						<label for="email">Email</label>
+					</div>
+				</div>
+				<div class="row">
+
+				</div>
+
+
+
+		</form>
+	</div>
+</body>
+
+</html>
